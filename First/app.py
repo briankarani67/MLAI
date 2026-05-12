9c7420b14e56613564e6e35e11dbc809f7f530e7
briankarani67 <briankarani884@gmail.com>
2026-05-12 05:44:45 +0300

    Improving the training in MLPClassifier

--- First/app.py
+++ First/app.py
@@ -23,7 +23,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
 )
 
-model = MLPClassifier(hidden_layer_sizes=(50,), max_iter=500, random_state=42)
+model = MLPClassifier(hidden_layer_sizes=(100,50), max_iter=1000, random_state=42)
 
 print("Training the model...")
 model.fit(X_train, y_train)
